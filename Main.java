22c11d4d3e19601a9faf2fca2d7dc25fccbe35d0
kilgn <karli@live.wsd1.org>
2024-05-09 15:29:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,21 @@ import java.util.ArrayList;
 import java.math.*;
 class Main 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) // [1,2,3,4] base 2
     {
 
         private int IterativeBaseConverter(ArrayList<Integer> digits, int base)
         {
-            return 7
+            int ans = 0;
+            for (int i = 0; i < digits.size(); i++;)
+            {
+                ans += digits[i] * Math.pow(base,digits.size()-i);
+            }
+            return ans
 
 
         }
 
+        
     }
 }
