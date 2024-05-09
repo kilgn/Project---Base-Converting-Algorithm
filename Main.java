a70e0b13f72c4611b95367de268486df9a33cb40
kilgn <karli@live.wsd1.org>
2024-05-09 18:12:49 -0500

    big progress

--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 import java.math.*;
 class Main 
 {
+    static int ans = 0;
     public static void main(String[] args)
     {
         ArrayList<Integer> digits = new ArrayList<Integer>();
         digits.add(1);
-        digits.add(2);
-        digits.add(0);
-        digits.add(5);
-        System.out.println(IterativeBaseConverter(digits, 9));
+        digits.add(1);
+        digits.add(1);
+        digits.add(1);
+
+        System.out.println(RecursiveBaseConverter(digits, 2));
        
 
         
@@ -18,7 +20,7 @@ class Main
     public static int IterativeBaseConverter(ArrayList<Integer> digits, int base) // Overall time complexity of O(n).
     {
         int ans = 0;
-        for (int i = 0; i < digits.size();i++) // O(n)
+        for (int i = 0; i < digits.size();i++) // O(n), depends on the size of my ArrayList.
         {
 
             ans += digits.get(i) * Math.pow(base,digits.size()-(i+1)); // O(1)
@@ -27,22 +29,25 @@ class Main
         return ans;
     }
 
+
     public static int RecursiveBaseConverter(ArrayList<Integer> digits, int base)
     {
-        int ans = 0;
-        if (digits.size() == 1)
+        if (digits.size() == 1) // O(n), depends on the size of my ArrayList.
         {
-            return ans += digits.get(0);
+            return ans += digits.get(0) * 1;
         }
-        else
-        {   // [1,2,3,4] base 2
-            ans += digits.get(0) * Math.pow(base,digits.size()-1);
-            
+        else // [1,1,1,1] base 2
+        {   
+            ans += digits.get(0) * Math.pow(base, digits.size()-1);
+            ArrayList<Integer> newDigits = digits;
+            newDigits.remove(0);
+            RecursiveBaseConverter(newDigits, base);
+            return ans;
         }
+        
 
-        return ans;
+        
 
     }
-
 
 }
