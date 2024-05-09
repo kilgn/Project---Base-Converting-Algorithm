10ed17c0d179c1aa86cf9fc9dffcfa4d9e45f63e
kilgn <karli@live.wsd1.org>
2024-05-09 15:18:12 -0500

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.math.*;
+class Main 
+{
+    public static void main(String[] args)
+    {
+
+        private int IterativeBaseConverter(ArrayList<Integer> digits, int base)
+        {
+            return 7
+
+
+        }
+
+    }
+}
