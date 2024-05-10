39cc4c4866b7273652b06dca69d47a86d07fd2c8
kilgn <karli@live.wsd1.org>
2024-05-10 13:40:39 -0500

    bad progress

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 import java.math.*;
 class Main 
 {
-    static int ans = 0;
     public static void main(String[] args)
     {
         ArrayList<Integer> digits = new ArrayList<Integer>();
-        digits.add(1);
-        digits.add(1);
-        digits.add(1);
-        digits.add(1);
+        digits.add(1); digits.add(2); digits.add(0); digits.add(5);
+        System.out.println(RecursiveBaseConverter(digits, 9));
 
-        System.out.println(RecursiveBaseConverter(digits, 2));
+
+        
        
 
         
@@ -19,29 +17,30 @@ class Main
     // Iterative base converter algorithm.
     public static int IterativeBaseConverter(ArrayList<Integer> digits, int base) // Overall time complexity of O(n).
     {
-        int ans = 0;
+        int answer = 0;
         for (int i = 0; i < digits.size();i++) // O(n), depends on the size of my ArrayList.
         {
 
-            ans += digits.get(i) * Math.pow(base,digits.size()-(i+1)); // O(1)
+            answer += digits.get(i) * Math.pow(base,digits.size()-(i+1)); // O(1)
             //This line accesses the "digits" ArrayList and multiplies this element with the base to the power respective to the position, and adds this value to "ans".
         }
-        return ans;
+        return answer;
     }
-
 
     public static int RecursiveBaseConverter(ArrayList<Integer> digits, int base)
     {
+        
         if (digits.size() == 1) // O(n), depends on the size of my ArrayList.
         {
-            return ans += digits.get(0) * 1;
+            return digits.get(0);
         }
-        else // [1,1,1,1] base 2
+        else // temp list [1,2,0,5]
         {   
-            ans += digits.get(0) * Math.pow(base, digits.size()-1);
+            int tempDigit = digits.get(0);
             ArrayList<Integer> newDigits = digits;
             newDigits.remove(0);
-            RecursiveBaseConverter(newDigits, base);
+            double tempAns = tempDigit * Math.pow(base, digits.size() - 1) + RecursiveBaseConverter(newDigits, base);
+            int ans = (int)tempAns;
             return ans;
         }
         
