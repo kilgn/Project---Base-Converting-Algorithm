f86544d93626a18825a547e8ed2f220bb18d4e78
kilgn <162633548+kilgn@users.noreply.github.com>
2024-05-16 12:54:50 -0500

    added some // O(1)

--- Main.java
+++ Main.java
@@ -22,14 +22,14 @@ class Main
     // Iterative base converter algorithm.
     public static int IterativeBaseConverter(ArrayList<Integer> digits, int base) // Overall time complexity of O(nlog(n)).
     {
-        int answer = 0;
+        int answer = 0; // O(1)
         for (int i = 0; i < digits.size();i++) // O(n), depends on the size of my ArrayList.
         {
 
             answer += digits.get(i) * Math.pow(base,digits.size()-(i+1)); // O(log(n))
             //This line accesses the "digits" ArrayList and multiplies this element with the base to the power respective to the position, and adds this value to "ans".
         }
-        return answer;
+        return answer; // O(1)
     }
     // Recursive base converter algorithm.
     public static int RecursiveBaseConverter(ArrayList<Integer> digits, int base) // Overall time complexity of O(log(n))
@@ -37,7 +37,7 @@ class Main
         
         if (digits.size() == 1) // O(1)
         {
-            return digits.get(0);
+            return digits.get(0); // O(1)
         }
         else 
         {   
